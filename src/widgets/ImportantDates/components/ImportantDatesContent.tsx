1a18c0a9753807476ad51f96a11f905bc3f93c56
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-08 23:14:25 +0530

    feat: important dates

--- src/widgets/ImportantDates/components/ImportantDatesContent.tsx
+++ src/widgets/ImportantDates/components/ImportantDatesContent.tsx
@@ -1,14 +1,24 @@
 import Titlebar from "@components/Titlebar";
+import Image from "next/image";
 import React from "react";
 
 export default function ImportantDatesContent() {
   return (
-    <div className="px-[5vw] min-h-[100vh] pt-[110px]">
+    <div className="px-[5vw] min-h-[100vh] pt-[110px] pb-5">
       <div className="">
         <div className="">
           <Titlebar
             title="Important Dates"
             className="text-2xl font-medium text-primary-600"
+          />
+        </div>
+        <div className="w-full items-center justify-center flex">
+          <Image
+            src={"/images/important.png"}
+            alt=""
+            width={1000}
+            height={1000}
+            className="h-full"
           />
         </div>
       </div>
