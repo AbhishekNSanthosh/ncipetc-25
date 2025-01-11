bc7d38e9c0216db6e6b2401db4ca2f3d6689d94c
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-11 10:15:59 +0530

    feat: menu

--- src/widgets/SubmissionGuidelines/components/SubmissionGuidelines.tsx
+++ src/widgets/SubmissionGuidelines/components/SubmissionGuidelines.tsx
@@ -6,19 +6,19 @@ import { HiOutlineRocketLaunch } from "react-icons/hi2";
 
 export default function SubmissionGuidelinesContent() {
   return (
-    <div className="px-[5vw] pt-[100px] pb-[5vh]">
-      <div className="flex flex-col space-y-8">
+    <div className="px-4 sm:px-[5vw] pt-[80px] lg:pt-[100px] pb-6 sm:pb-[5vh]">
+      <div className="flex flex-col space-y-6 sm:space-y-8">
         <div className="">
           <Titlebar
-            className="text-2xl font-medium text-primary-600"
+            className="lg:text-2xl text-base sm:text-lg font-medium text-primary-600"
             title="Submission Guidelines"
           />
         </div>
-        <div className="flex flex-col space-y-5">
-          <span className="font-semibold text-xl text-gray-800">
+        <div className="flex flex-col space-y-4 sm:space-y-5">
+          <span className="font-semibold text-lg sm:text-xl text-gray-800">
             Paper Submission
           </span>
-          <p className="text-justify text-lg text-gray-700">
+          <p className="text-justify text-base sm:text-lg text-gray-700">
             Authors are invited to submit papers in electronic format.
             Instructions are published in the conference website. Acceptance or
             rejection of papers is based on peer review of the full papers. The number of authors and
@@ -36,28 +36,31 @@ export default function SubmissionGuidelinesContent() {
             .
           </p>
         </div>
-        <div className="flex flex-col space-y-5">
-          <span className="font-semibold text-xl text-gray-800">AUTHORS</span>
-          <p className="text-justify text-lg text-gray-700">
+        <div className="flex flex-col space-y-4 sm:space-y-5">
+          <span className="font-semibold text-lg sm:text-xl text-gray-800">AUTHORS</span>
+          <p className="text-justify text-base sm:text-lg text-gray-700">
             Authors who wish to participate at the conference must register
-            individually. The selected paper requires atleast one author to
+            individually. The selected paper requires at least one author to
             attend the conference for presentation. Papers not presented at the
             conference will not be included in the Conference Proceedings.
           </p>
-          <div className="flex flex-col space-y-5">
+          <div className="flex flex-col space-y-3 sm:space-y-5">
             {authorGuidelines?.map((item, index) => (
-              <span className="text-justify text-lg text-gray-700 flex items-center space-x-2 gap-3">
-                <HiOutlineRocketLaunch />
+              <span
+                key={index}
+                className="text-justify text-base sm:text-lg text-gray-700 flex items-center space-x-2 gap-3"
+              >
+                <HiOutlineRocketLaunch className="text-[35px] lg:text-xl"/>
                 {item}
               </span>
             ))}
           </div>
         </div>
-        <div className="flex flex-col space-y-5">
-          <span className="font-semibold text-xl text-gray-800">
+        <div className="flex flex-col space-y-4 sm:space-y-5">
+          <span className="font-semibold text-lg sm:text-xl text-gray-800">
             PEER REVIEW
           </span>
-          <p className="text-justify text-lg text-gray-700">
+          <p className="text-justify text-base sm:text-lg text-gray-700">
             Accepted papers will be subjected to a double-blind review process
             conducted by the Program Committee. This evaluation will be based on
             criteria such as technical soundness, alignment with the
@@ -70,28 +73,28 @@ export default function SubmissionGuidelinesContent() {
             undergoing the review process.
           </p>
         </div>
-        <div className="flex flex-col space-y-5">
-          <span className="font-semibold text-xl text-gray-800">
+        <div className="flex flex-col space-y-4 sm:space-y-5">
+          <span className="font-semibold text-lg sm:text-xl text-gray-800">
             FINAL NOTIFICATION
           </span>
-          <p className="text-justify text-lg text-gray-700">
+          <p className="text-justify text-base sm:text-lg text-gray-700">
             The notification will be emailed to the author based on the
             reviewers’ feedback. The Notification will be e-mailed to authors
             under any of the three categories
           </p>
-          <div className="flex flex-col space-y-5">
-            <span className="text-gray-700 text-lg">1. Accepted</span>
-            <span className="text-gray-700 text-lg">
+          <div className="flex flex-col space-y-2 sm:space-y-5">
+            <span className="text-gray-700 text-base sm:text-lg">1. Accepted</span>
+            <span className="text-gray-700 text-base sm:text-lg">
               2. May be accepted with Minor/Major changes
             </span>
-            <span className="text-gray-700 text-lg">3. Rejected</span>
+            <span className="text-gray-700 text-base sm:text-lg">3. Rejected</span>
           </div>
         </div>
-        <div className="flex flex-col space-y-5">
-          <span className="font-semibold text-xl text-gray-800">
+        <div className="flex flex-col space-y-4 sm:space-y-5">
+          <span className="font-semibold text-lg sm:text-xl text-gray-800">
             PLAGIARISM
           </span>
-          <p className="text-justify text-lg text-gray-700">
+          <p className="text-justify text-base sm:text-lg text-gray-700">
             Plagiarism in conferences involves presenting someone else's work,
             ideas, or words as one's own without proper credit. It can include
             direct copying, paraphrasing without attribution, self-plagiarism,
@@ -101,11 +104,11 @@ export default function SubmissionGuidelinesContent() {
             penalties.
           </p>
         </div>
-        <div className="flex flex-col space-y-5">
-          <span className="font-semibold text-xl text-gray-800">
+        <div className="flex flex-col space-y-4 sm:space-y-5">
+          <span className="font-semibold text-lg sm:text-xl text-gray-800">
             GENERAL GUIDELINES
           </span>
-          <p className="text-justify text-lg text-gray-700">
+          <p className="text-justify text-base sm:text-lg text-gray-700">
             Plagiarism in conferences involves presenting someone else's work,
             ideas, or words as one's own without proper credit. It can include
             direct copying, paraphrasing without attribution, self-plagiarism,
@@ -114,15 +117,15 @@ export default function SubmissionGuidelinesContent() {
             thorough reviews. Violations can lead to rejection or other
             penalties.
           </p>
-          <div className="flex flex-col space-y-5">
-            <span className="text-justify text-lg text-gray-700 flex items-center gap-3">
-              <HiOutlineRocketLaunch />
+          <div className="flex flex-col space-y-3 sm:space-y-5">
+            <span className="text-justify text-base sm:text-lg text-gray-700 flex items-center gap-3">
+            <HiOutlineRocketLaunch className="text-[35px] lg:text-xl"/>
               Final camera ready paper (It should be in editable format either
               word or latex file)
             </span>
-            <div className="flex items-center gap-3">
-              <HiOutlineRocketLaunch />
-              <span className="text-justify text-lg text-gray-700">
+            <div className="flex flex-row items-start sm:items-center gap-3">
+            <HiOutlineRocketLaunch className="text-[35px] lg:text-xl"/>
+              <span className="text-justify text-base sm:text-lg text-gray-700">
                 Copyright agreement form (It should be duly signed by the
                 corresponding author with date). Copyright agreement template
                 can be <br />
@@ -137,8 +140,8 @@ export default function SubmissionGuidelinesContent() {
                 </Link>
               </span>
             </div>
-            <span className="text-justify text-lg text-gray-700 flex items-center gap-3">
-              <HiOutlineRocketLaunch /> Payment Proof (Anyone of the author can
+            <span className="text-justify text-base sm:text-lg text-gray-700 flex items-center gap-3">
+            <HiOutlineRocketLaunch className="text-[35px] lg:text-xl"/> Payment Proof (Anyone of the author can
               pay - Registration fee per paper only).
             </span>
           </div>
