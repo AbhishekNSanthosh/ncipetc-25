dda891d12e056a3bffd707e028bc043c22422b47
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-12 21:32:06 +0530

    feat:seo

--- src/app/organizers/page.tsx
+++ src/app/organizers/page.tsx
@@ -1,5 +1,11 @@
 import Organizers from '@widgets/Organizers'
 import React from 'react'
+export const metadata = {
+  title: "Organizers | NCIPETC-25",
+  description:
+    "Meet the dedicated organizers behind NCIPETC-25, the National Conference on Interdisciplinary Perspectives of Emerging Trends in Computing, hosted by Carmel College of Engineering and Technology.",
+};
+
 
 export default function page() {
   return (
