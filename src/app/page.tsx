cbf9a7618c5174b2423f2f858336f11d077ec833
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-12 21:34:39 +0530

    feat:seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import LandingPageView from '@widgets/LandingPage'
 import React from 'react'
-
+export const metadata = {
+  title: "Home | NCIPETC-25",
+  description:
+    "Welcome to NCIPETC-25, the National Conference on Interdisciplinary Perspectives of Emerging Trends in Computing. Join us at Carmel College of Engineering and Technology for groundbreaking discussions and innovations.",
+};
 export default function Home() {
   return (
     <LandingPageView/>
