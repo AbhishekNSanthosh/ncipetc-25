0ba64cdc186d14fdda61d0755b6699f0a75c3f77
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-12 21:29:20 +0530

    feat:seo

--- src/app/advisory-board/page.tsx
+++ src/app/advisory-board/page.tsx
@@ -1,5 +1,11 @@
 import AdvisoryBoard from '@widgets/AdvisoryBoard'
 import React from 'react'
+export const metadata = {
+  title: "Advisory Board | NCIPETC-25",
+  description:
+    "Meet the distinguished Advisory Board members of NCIPETC-25, who bring their expertise and vision to guide the National Conference on Interdisciplinary Perspectives of Emerging Trends in Computing.",
+};
+
 
 export default function page() {
   return (
