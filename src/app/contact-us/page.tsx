a66bbfed5f90864bedede190d3bb7b52a8870af3
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-12 21:30:37 +0530

    feat:seo

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,5 +1,11 @@
 import Contact from '@widgets/Contact'
 import React from 'react'
+export const metadata = {
+  title: "Contact Us | NCIPETC-25",
+  description:
+    "Get in touch with us for any queries or information about NCIPETC-25, the National Conference on Interdisciplinary Perspectives of Emerging Trends in Computing.",
+};
+
 
 export default function page() {
   return (
