26b4fbe5e5dfe4639d487185769d92bcddd9cf44
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-12 21:31:18 +0530

    feat:seo

--- src/app/important-dates/page.tsx
+++ src/app/important-dates/page.tsx
@@ -1,5 +1,11 @@
 import ImportantDates from '@widgets/ImportantDates'
 import React from 'react'
+export const metadata = {
+  title: "Important Dates | NCIPETC-25",
+  description:
+    "Stay updated with the important dates for NCIPETC-25, the National Conference on Interdisciplinary Perspectives of Emerging Trends in Computing hosted by Carmel College of Engineering and Technology.",
+};
+
 
 export default function page() {
   return (
