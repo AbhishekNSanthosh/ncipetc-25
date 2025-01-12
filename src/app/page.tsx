13617e9c0a31735eab4b7d039f1abf5784ade32b
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-12 21:34:52 +0530

    feat:seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export const metadata = {
   description:
     "Welcome to NCIPETC-25, the National Conference on Interdisciplinary Perspectives of Emerging Trends in Computing. Join us at Carmel College of Engineering and Technology for groundbreaking discussions and innovations.",
 };
+
 export default function Home() {
   return (
     <LandingPageView/>
