0b7c8fa1e0683abfd2c4e77df020ca92d30b5431
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-12 21:33:38 +0530

    feat:seo

--- src/app/submission-guidelines/page.tsx
+++ src/app/submission-guidelines/page.tsx
@@ -1,5 +1,6 @@
 import SubmissionGuidelines from '@widgets/SubmissionGuidelines'
 import React from 'react'
+
 export const metadata = {
   title: "Submission Guidelines | NCIPETC-25",
   description:
