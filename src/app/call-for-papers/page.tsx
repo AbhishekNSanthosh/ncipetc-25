f9825b6c75ba545ee8675f493c70cf5de883c792
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-12 21:30:00 +0530

    feat:seo

--- src/app/call-for-papers/page.tsx
+++ src/app/call-for-papers/page.tsx
@@ -1,5 +1,11 @@
 import CallForPapers from '@widgets/CallForPaper'
 import React from 'react'
+export const metadata = {
+  title: "Call for Papers | NCIPETC-25",
+  description:
+    "Submit your research to NCIPETC-25! Explore interdisciplinary perspectives on emerging trends in computing at the National Conference hosted by Carmel College of Engineering and Technology.",
+};
+
 
 export default function page() {
   return (
