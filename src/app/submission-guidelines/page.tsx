0827af6111f5d1d233340b8623b603aecd622eb3
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-12 21:33:33 +0530

    feat:seo

--- src/app/submission-guidelines/page.tsx
+++ src/app/submission-guidelines/page.tsx
@@ -1,5 +1,11 @@
 import SubmissionGuidelines from '@widgets/SubmissionGuidelines'
 import React from 'react'
+export const metadata = {
+  title: "Submission Guidelines | NCIPETC-25",
+  description:
+    "Follow the submission guidelines for NCIPETC-25, the National Conference on Interdisciplinary Perspectives of Emerging Trends in Computing. Ensure your paper meets all the requirements for submission.",
+};
+
 
 export default function page() {
   return (
